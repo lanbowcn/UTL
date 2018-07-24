4ae1a0b732180a073468eb7d7089563f9072518a
ben's w-laptop <lanbow@live.cn>
2018-07-25 00:50:43 +0800

    增加判断消除 ；空白的输出（带冒号）

--- tool/Grd.py
+++ tool/Grd.py
@@ -24,7 +24,8 @@ def read_excel():
             valcell = table.cell_value(valrow, i)
             if ctype == 2 and valcell % 1 == 0.0:
                 valcell = int(valcell)
-            print(str(keycell)+':'+str(valcell)+',',end='')
+            if keycell != '':
+                print(str(keycell)+':'+str(valcell)+',',end='')
         print(' ')
         print('    我校暑假时间为2018年7月16日—8月31日，学生9月1日、2日回校报到注册，9月3日正式上课。')
         print('    补考安排在下学期开学前一周进行，即在星期四、五、六、日四天（8月30～9月2日），有不及格科目的学生，应利用假期认真复习备考，提前回来参加补考，以取得毕业学分。')
