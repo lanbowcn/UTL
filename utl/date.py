6ccd63beff899226367712924e194a7a18c2ce60
ben's w-laptop <lanbow@live.cn>
2018-09-14 09:57:41 +0800

    增加判断消除 ；空白的输出（带冒号）

--- utl/date.py
+++ utl/date.py
@@ -1,5 +1,11 @@
 import datetime
+import pandas as pd
 #对日期字符进行读取，及变换
 a="2003/10/10 16:10:00"
 b=datetime.datetime.strptime(a,"%Y/%m/%d %H:%M:%S")
 print(b.strftime('%Y-%m'))
+
+#将日期作为index进行排序
+data = pd.read_csv('../dataset2/jdt.csv', index_col='commitdate', parse_dates=True).sort_index()
+
+df = pd.read_table('G:/tc/dataset/user_view.txt', sep=",")
