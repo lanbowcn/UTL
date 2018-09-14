b59f1a6b25181bf5309b463880521dea216c117c
lanbowcn <lanbow@live.cn>
2018-09-14 17:42:31 +0800

    sdp

--- utl/date.py
+++ utl/date.py
@@ -9,3 +9,11 @@ print(b.strftime('%Y-%m'))
 data = pd.read_csv('../dataset2/jdt.csv', index_col='commitdate', parse_dates=True).sort_index()
 
 df = pd.read_table('G:/tc/dataset/user_view.txt', sep=",")
+
+# df_period = df.to_period('M')  # 按月显示，但不统计
+# print(type(df_period.index))
+# unit = [i for i in df_period.index]
+# print(unit)
+
+# for str in data['commitdate']:
+# np.Series a = datetime.datetime.strptime(data['commitdate'],"%Y/%m/%d  %H:%M:%S")
