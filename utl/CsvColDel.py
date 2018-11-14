86281967601f2613ec6034c2578f7a6d64346157
ben's w-laptop <lanbow@live.cn>
2018-11-14 23:39:17 +0800

    最终数据集处理 11.14

--- utl/CsvColDel.py
+++ utl/CsvColDel.py
@@ -29,43 +29,47 @@ def del_cvs_col(fname, newfname, idxs, delimiter=' '):
 
 
 
+def dataPro(input):
+    input_file = open(r"input/"+input)
+    output_file = open("output/"+input,"w",newline='')
+    table = []
+    header = input_file.readline() #读取并弹出第一行
+    header = header.strip('\n')
+    header = header.split(',')
+    header = header[3:]
+    header.insert(0,'fileID')
+    header.insert(-1,'defect')
+    print(header)
 
-input_file = open("ant-1.7.csv")
-output_file = open("b.csv","w",newline='')
-table = []
-header = input_file.readline() #读取并弹出第一行
-header = header.strip('\n')
-header = header.split(',')
-header = header[3:]
-header.insert(0,'fileID')
-header.insert(-1,'defect')
-print(header)
+    i = 0
+    for line in input_file:
+        line = line.strip('\n')
+        col = line.split(',')
+        col = col[3:]
+        if col[-1]!='0':
+            col.insert(-1,'1')
+        else:
+            col.insert(-1,'0')
+        col.insert(0,i)
+        i=i+1
+        print(col)
+        table.append(col)
 
-i = 0
-for line in input_file:
-    line = line.strip('\n')
-    col = line.split(',')
-    col = col[3:]
-    if col[-1]!='0':
-        col.insert(-1,'1')
-    else:
-        col.insert(-1,'0')
-    col.insert(0,i)
-    i=i+1
-    print(col)
-    table.append(col)
+    # output_file.write(header+'\t')
+    writer = csv.writer(output_file)
+    writer.writerow(header)
+    for row in table:
+        row = [str(x) for x in row]
+        # output_file.write("\t".join(row)+'\n')
+        writer.writerow(row)
 
-# output_file.write(header+'\t')
-writer = csv.writer(output_file)
-writer.writerow(header)
-for row in table:
-    row = [str(x) for x in row]
-    # output_file.write("\t".join(row)+'\n')
-    writer.writerow(row)
+    input_file.close()
+    output_file.close()
 
-input_file.close()
-output_file.close()
-
+dataSet = ["ant-1.7.csv","camel-1.6.csv","ivy-1.4.csv","jedit-4.0.csv","log4j-1.0.csv","poi-2.0.csv",
+           "tomcat.csv","velocity-1.6.csv","xalan-2.4.csv","xerces-1.3.csv"]
+for i in dataSet:
+    dataPro(i)
 
 
 
